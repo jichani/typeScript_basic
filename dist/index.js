0e0e56d987844310dcc957b0d1ad189d7185706b
Jichani <123962013+jichani@users.noreply.github.com>
2024-03-25 20:54:15 +0900

    타입 스크립트 공부 중

--- dist/index.js
+++ dist/index.js
@@ -7,16 +7,29 @@
 // any(기타)(잘사용하진 않는다.)
 let a = 3;
 let b = "love you";
+// 아무 타입이나 정해주는 것도 가능하다.
 let c = 3;
 c = "asd";
-// 두가지 타입을 정해주는 것도 가능하다.
+// 다양한 타입을 정해주는 것도 가능하다. (Union type)
 let d = "asdsd";
 d = 123;
+// 배열을 지정해주는 경우
 let e = ["apple", "banana"];
 let f = [3, 6];
+// 객체를 지정해주는 경우 (?는 옵션)
+let g = {};
+// 함수의 파라미터의 타입과 리턴 타입을 정할 수도 있다.
 function addNumber(a, b) {
     return a + b;
 }
 addNumber(3, 7);
-// tsc index.ts
+let h = 123;
+let john = [123, true];
+let john2 = { name: 'kim', age: '123' };
+// class도 타입 지정 가능
+class User {
+    constructor(name) {
+        this.name = name;
+    }
+}
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -9,20 +9,49 @@
 let a:number = 3;
 let b:string = "love you";
 
+// 아무 타입이나 정해주는 것도 가능하다.
 let c:any = 3;
 c = "asd";
 
-// 두가지 타입을 정해주는 것도 가능하다.
+// 다양한 타입을 정해주는 것도 가능하다. (Union type)
 let d : number | string = "asdsd";
 d = 123;
 
+// 배열을 지정해주는 경우
 let e:string[] = ["apple", "banana"];
 
 let f:number[] = [3, 6];
 
+// 객체를 지정해주는 경우 (?는 옵션)
+let g:{name? : string} = {}
+
+// 함수의 파라미터의 타입과 리턴 타입을 정할 수도 있다.
 function addNumber (a:number, b:number):number {
   return a + b;
 }
 
 addNumber(3, 7);
-// tsc index.ts
+
+// 타입을 변수에 담아서도 쓸 수 있다. (Type alias) (타입 작명은 대문자를 사용한다.)
+type StringAndNumber = string | number;
+
+let h : StringAndNumber = 123;
+
+// array에 쓸 수 있는 tuple 타입
+type Member = [number, boolean];
+let john:Member = [123, true];
+
+// object에 타입지정해야할 속성이 너무 많으면
+type Members = {
+  [key: string] : string,
+}
+
+let john2 : Members = { name: 'kim', age: '123'};
+
+// class도 타입 지정 가능
+class User {
+  name : string;
+  constructor(name : string) {
+    this.name = name;
+  }
+}
